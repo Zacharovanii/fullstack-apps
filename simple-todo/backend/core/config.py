98a754ac6ada4f0ba5eefddb4b28c4dd4fb79598
Zacharovanii <zacharovaniiDev@proton.me>
2026-01-08 16:51:50 +0300

    create db helper for connection with db

--- simple-todo/backend/core/config.py
+++ simple-todo/backend/core/config.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 from pathlib import Path
 
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 PROJECT_PATH = Path(__file__).parents[1].resolve()
@@ -20,6 +20,22 @@ class LoggingConfig(BaseModel):
         return path / self.file_name
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class AppConfig(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(
@@ -34,6 +50,7 @@ class AppConfig(BaseSettings):
     )
 
     logging: LoggingConfig = LoggingConfig()
+    db: DatabaseConfig
 
 
 @lru_cache
--- simple-todo/backend/main.py
+++ simple-todo/backend/main.py
@@ -3,16 +3,21 @@ from fastapi import FastAPI
 from loguru import logger
 
 from core import setup_logging
+from db import db_helper
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(fs: FastAPI):
+    logger.success("BACKEND STARTING UP")
+    yield
+    await db_helper.dispose()
+    logger.info("BACKEND SHUTDOWN")
+
+
+app = FastAPI(lifespan=lifespan)
 setup_logging()
 
 
-def main():
-    logger.success("BACKEND STARTING UP")
-    logger.warning("TEST")
-    uvicorn.run("main:app", access_log=True)
-
-
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", access_log=True)
